36b71686305103d04da8918dc2ca622675e2840e
haimshalev <haimshalev@gmail.com>
2014-05-10 18:18:06 +0300

    BamNeuralNetworkProjects updates: Added the BamNeuralNetwork class which implemnts a bidirectional bipolar neuarl network. Updated the program to use the Bam network with some random bipolar vectors

---
+++ BamPhoneNumbersFrom16BitIcons/BamNeuralNetwork.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Linq;
+
+namespace BamPhoneNumbersFrom16BitIcons
+{
+    class BamNeuralNetwork
+    {
+        private readonly int _numberOfInputNeurons;
+        private readonly int _numberOfOutputNeurons;
+        private readonly double[][] _transformationMatrix;
+        private readonly double[][] _transformationMatrixReverse;
+
+        private const int Threshold = 0;
+
+        /// <summary>
+        /// Cto'r
+        /// </summary>
+        /// <param name="numberOfInputNeurons">the size of the input vectors</param>
+        /// <param name="numberOfOutputNeurons">the size of the output vectors</param>
+        public BamNeuralNetwork(int numberOfInputNeurons, int numberOfOutputNeurons)
+        {
+            // set internal properties
+            _numberOfInputNeurons = numberOfInputNeurons;
+            _numberOfOutputNeurons = numberOfOutputNeurons;
+
+            // initializing the transformation matrices
+            _transformationMatrix = new double[_numberOfInputNeurons][];
+            for (var i = 0; i < _numberOfInputNeurons; i++)
+                _transformationMatrix[i] = new double[_numberOfOutputNeurons];
+
+            _transformationMatrixReverse = new double[_numberOfOutputNeurons][];
+            for (var i = 0; i < _numberOfOutputNeurons; i++)
+                _transformationMatrixReverse[i] = new double[_numberOfInputNeurons];
+        }
+
+        /// <summary>
+        /// Add an association to the neural network and updates the inner transformation matrix
+        /// </summary>
+        /// <param name="input">input vector</param>
+        /// <param name="output">output vector</param>
+        public void AddAssociation(int[] input, int[] output)
+        {
+            if (input.Length != _numberOfInputNeurons || output.Length != _numberOfOutputNeurons)
+                throw new ArgumentException("AddAssociation filed: one of the input or output vectors is from incorrect size");
+
+            // update the values of the transformation matrix
+            for (var i = 0; i < _numberOfInputNeurons; i++)
+                for (var j = 0; j < _numberOfOutputNeurons; j++)
+                {
+                    _transformationMatrix[i][j] += input[i] * output[j];
+                    _transformationMatrixReverse[j][i] += input[i] * output[j];
+                }
+        }
+
+        /// <summary>
+        /// Clear the inner transformation matrix
+        /// </summary>
+        public void RemoveAssociations()
+        {
+            for (var i = 0; i < _numberOfInputNeurons; i++)
+                for (var j = 0; j < _numberOfOutputNeurons; j++)
+                {
+                    _transformationMatrix[i][j] = 0;
+                    _transformationMatrixReverse[j][i] = 0;
+                }
+        }
+
+        /// <summary>
+        /// output the transformation matrix (weight matrix) to the console
+        /// </summary>
+        public void PrintTransformationMatrix()
+        {
+            Console.WriteLine("The transformation matrix:");
+            for (var i = 0; i < _numberOfInputNeurons; i++)
+            {
+                for (var j = 0; j < _numberOfOutputNeurons; j++)
+                    Console.Write(_transformationMatrix[i][j] + " ");
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// Associate the input vector to one of the stored associations
+        /// </summary>
+        /// <param name="input">the input vector to associate</param>
+        /// <param name="output">optional parameter, the desired output vector</param>
+        public void Associate(int[] input, int[] output)
+        {
+            var isForwardStable = false;
+            var isBackwardStable = false;
+
+            // while the 2 vectors are unstable (not changing from iteration to iteration) 
+            while (!isBackwardStable && !isForwardStable)
+            {
+                // propagate forward and propagate backward
+                isForwardStable = PropagateLayer(_transformationMatrix, input, output);
+                isBackwardStable = PropagateLayer(_transformationMatrixReverse, output, input);
+            }
+        }
+
+        /// <summary>
+        /// propagate the input vector to the output layer using the given transformation matrix
+        /// </summary>
+        /// <param name="transformationMatrix">the transformation matrix to the other layer</param>
+        /// <param name="input">the input vector</param>
+        /// <param name="output">the current association</param>
+        /// <returns>true if the output vector stays the same</returns>
+        private bool PropagateLayer(double[][] transformationMatrix, int[] input, int[] output)
+        {
+            // initialize the network to stable
+            var stable = true;
+
+            // run over all the output values
+            for (var i = 0; i < output.Length; i++)
+            {
+                // vector multiplication with the correct matrix
+                int sum;
+                if (input.Length != transformationMatrix.Length)
+                    sum = (int) input.Select((t, j) => transformationMatrix[i][j]*t).Sum();
+                else
+                    sum = (int) input.Select((t, j) => transformationMatrix[j][i]*t).Sum();
+                
+                // threshold the output
+                var outputVal = Sign(sum);
+
+                // if we didn't get the desired output, unstable
+                if (outputVal != output[i])
+                {
+                    stable = false;
+                    output[i] = outputVal;
+                }
+            }
+
+            return stable;
+        }
+
+        /// <summary>
+        /// return 1 if the input value greater then the threshold, else -1
+        /// </summary>
+        /// <param name="x">input value</param>
+        /// <param name="threshold">checked threshold</param>
+        /// <returns>1 if the input value greater then the threshold, else -1</returns>
+        private static int Sign(double x, int threshold = Threshold)
+        {
+            if (x > threshold) return 1;
+            return -1;
+        }
+    }
+}
--- BamPhoneNumbersFrom16BitIcons/Program.cs
+++ BamPhoneNumbersFrom16BitIcons/Program.cs
@@ -1,9 +1,55 @@
-﻿namespace BamPhoneNumbersFrom16BitIcons
+﻿using System;
+
+namespace BamPhoneNumbersFrom16BitIcons
 {
     class Program
     {
+        public const int NumberInputNeurons = 15;
+        public const int NumberOutputNeurons = 3;
+        public const int NumberTrainingPoints = 2;
+
         static void Main()
         {
+            #region Prepare the data
+
+            // initializing input vectors
+            var inputVectors = new int[NumberTrainingPoints][];
+            inputVectors[0] = new[] { 1, 1, -1, 1, 1, -1, 1, 1, -1, 1, 1, -1, 1, 1, -1 };
+            inputVectors[1] = new[] { -1, -1, -1, -1, 1, -1, -1, 1, -1, -1, 1, -1, -1, -1, -1 };
+
+            // initialize output vectors 
+            var outputVectors = new int[NumberTrainingPoints][];
+            outputVectors[0] = new[] { 1, 1, -1 };
+            outputVectors[1] = new[] { 1, 1, 1 }; 
+
+            // TODO: need to convert the data to biPolar
+
+            #endregion
+
+            // initialize the BAM neural network
+            var bamNeuralNetwork = new BamNeuralNetwork(NumberInputNeurons, NumberOutputNeurons);
+            
+            // add the associations to the network
+            for (var i = 0; i < NumberTrainingPoints; i++)
+                bamNeuralNetwork.AddAssociation(inputVectors[i], outputVectors[i]);
+
+            #region Retrival
+
+            // setting new inputs
+            var testVector = new[] { 1, 1, 1, 1, 1, 1, 1, 1, -1, 1, 1, -1, 1, 1, -1 };
+
+            // TODO: create random biPolar output vector
+            var outputVector = new[] {-1, -1, 1};
+
+            // Associate the test vector
+            bamNeuralNetwork.Associate(testVector , outputVector);
+
+            // output the result vector
+            Console.WriteLine("output vector : " + String.Join(",",outputVector));
+
+            #endregion
         }
     }
 }
+
+
