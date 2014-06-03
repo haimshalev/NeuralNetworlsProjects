60a98108660b3dbcc009feb15088304e3a121dcc
haimshalev <haimshalev@gmail.com>
2014-06-03 23:04:16 +0300

    Project3: Added Gui controls and completed the basic algorithm

--- SelfOrgenizedMap/MainWindow.xaml.cs
+++ SelfOrgenizedMap/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.Eventing.Reader;
+﻿using System;
+using System.Globalization;
 using System.Threading;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -6,52 +7,168 @@ using System.Windows.Shapes;
 
 namespace SelfOrgenizedMap
 {
+
+    public delegate void UpdateGuiDelegate(double[][] weights, double learnRate, int epoch);
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow
     {
+        private SelfOrgnizedMap _selfOrgenizedMap;
+        private double[][] _data;
+        private Thread _workingThread;
+
         public MainWindow()
         {
             InitializeComponent();
-
-            // Initialize Self Orgenized map 
-            var selfOrgenizedMap = new SelfOrgnizedMap(2, 10, this);
-
-            // prepare the data
-
-            // learn
-            selfOrgenizedMap.Learn(inputData, 100, 5);
         }
 
-
-        public void DrawOnCanvas(Color color, int x, int y)
+        /// <summary>
+        /// Draw a point on the main canvas
+        /// </summary>
+        /// <param name="point">the point to draw. a double array of length 2</param>
+        /// <param name="color">the color of the point</param>
+        /// <param name="size">size of rect around the point</param>
+        public void DrawPointOnCanvas(double[] point, Color color, int size)
         {
             // Draw rect
             var rect = new Rectangle
             {
                 Stroke = new SolidColorBrush(color),
-                Width = 15,
-                Height = 15
+                Width = size,
+                Height = size
             };
-            Canvas.SetLeft(rect, x);
-            Canvas.SetTop(rect, y);
+            Canvas.SetLeft(rect, point[0] - size/2 );
+            Canvas.SetTop(rect, point[1] - size/2);
+
             MainCanvas.Children.Add(rect);
+        }
+
+        /// <summary>
+        /// Draw a line on the main canvas
+        /// </summary>
+        /// <param name="point1">the start point of the line</param>
+        /// <param name="point2">the end point of the line</param>
+        /// <param name="color">the color of the brush of the line</param>
+        /// <param name="size">the stroke thickness of the line</param>
+        public void DrawLineOnCanvas(double[] point1, double[] point2, Brush color, int size)
+        {
+            var line = new Line
+            {
+                Stroke = color,
+                X1 = point1[0],
+                Y1 = point1[1],
+                X2 = point2[0],
+                Y2 = point2[1],
+                StrokeThickness = size
+            };
+            MainCanvas.Children.Add(line);
         }
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="weights"></param>
-        internal void UpdateWindow(double[][] weights)
+        /// <param name="learnRate"></param>
+        /// <param name="epochNum"></param>
+        internal void UpdateWindow(double[][] weights, double learnRate, int epochNum)
         {
+            if (!Dispatcher.CheckAccess())
+            {
+                // we were called on a worker thread
+                // marshal the call to the user interface thread
+                Dispatcher.Invoke(new UpdateGuiDelegate(UpdateWindow),
+                            new object[] { weights, learnRate, epochNum});
+                return;
+            }
+
+            // Clear the canvas from the previous update
+            MainCanvas.Children.Clear();
+
+            // Draw the data points
+            foreach (var d in _data)
+                DrawPointOnCanvas(d, Colors.Red, 1);
+
             // Draw all the neurons on the canvas
             foreach (var w in weights)
-                DrawOnCanvas(Colors.Black, (int)w[0], (int)w[1]);
+                DrawPointOnCanvas(w, Colors.Black, 3);
 
-            // stoll the gui for the user to see results
-            // TODO: replace it by a button press
-            Thread.Sleep(1000);
+            // Draw the lines between neurons
+            for (var i = 0; i < weights.Length - 1; i++)
+                DrawLineOnCanvas(weights[i], weights[i + 1], Brushes.Navy, 1);
+
+            // update the labels
+            LearnRate.Text = learnRate.ToString(CultureInfo.InvariantCulture);
+            EpochNum.Text = epochNum.ToString(CultureInfo.InvariantCulture);
+        }
+
+        /// <summary>
+        /// Start learning on different thread
+        /// </summary>
+        /// <param name="data">the data to learn</param>
+        /// <param name="nn">the SelfOrgenizedMap instance</param>
+        private void StartWorking(double[][] data, SelfOrgnizedMap nn)
+        {
+            if (_workingThread != null) _workingThread.Abort();
+
+            _data = data;
+            _selfOrgenizedMap = nn;
+
+            _selfOrgenizedMap.Data = _data;
+            _selfOrgenizedMap.MainWindowRefreshRate = int.Parse(SetRefreshRate.Text);
+            _selfOrgenizedMap.NumEpoches = int.Parse(SetNumEpoches.Text);
+
+            // learn
+            _workingThread = new Thread(_selfOrgenizedMap.Learn);
+            _workingThread.Start();
+        }
+
+        /// <summary>
+        /// Som neural network , line of neurons topology, uniform density data
+        /// </summary>
+        private void UnifromStartButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+            // prepare the data - ordered data
+            var dataSize = (int)(MainCanvas.Height * MainCanvas.Width);
+            
+            var data = new double[dataSize][];
+            for (var i = 0; i < MainCanvas.Height; i++)
+                for (var j = 0; j < MainCanvas.Width; j++)
+                {
+                    data[(i * ((int)MainCanvas.Width)) + j] = new double[] { i, j };
+                }
+
+            // Initialize Self Orgenized map 
+            var selfOrgenizedMap = new SelfOrgnizedMap(inputDimension: 2, numberOfClusters: 100, window: this);
+
+            StartWorking(data, selfOrgenizedMap);
+        }
+
+        /// <summary>
+        /// Som neural network, line of neurons topology, non uniform density data
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NonUniformStartButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var rand = new Random();
+
+            // prepare the data - random data
+            var dataSize = (int)(MainCanvas.Height * MainCanvas.Width);
+            var data = new double[dataSize][];
+            for (var i = 0; i < MainCanvas.Height; i++)
+                for (var j = 0; j < MainCanvas.Width; j++)
+                {
+                    data[(i * ((int)MainCanvas.Width)) + j] = new []
+                    {rand.Next((int) MainCanvas.Width/2), rand.Next((int) MainCanvas.Height/2) + MainCanvas.Height/2};
+                }
+
+            // Initialize Self Orgenized map 
+            var selfOrgenizedMap = new SelfOrgnizedMap(inputDimension: 2, numberOfClusters: 100, window: this);
+
+            StartWorking(data, selfOrgenizedMap);
         }
     }
 }
--- SelfOrgenizedMap/SelfOrgenizedMap.cs
+++ SelfOrgenizedMap/SelfOrgenizedMap.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace SelfOrgenizedMap
@@ -10,16 +9,50 @@ namespace SelfOrgenizedMap
     [Serializable]
     public class SelfOrgnizedMap 
     {
+        #region Properties
+
         /// <summary>
         /// The weights of the output set to the coordinates of each neuron
         /// neurons.
         /// </summary>
         private readonly double[][] _weights;
 
-        private readonly int _inputNeuronsCount;
+        /// <summary>
+        /// the biases for each neuron - used for not selecting the same neuron all the time
+        /// </summary>
+        private readonly double[] _biases;
+
+        /// <summary>
+        /// the dimension of each datapoint
+        /// </summary>
+        private readonly int _inputDimension;
+
+        /// <summary>
+        /// the number of Kohonen neurons (number of clasters)
+        /// </summary>
         private readonly int _outputNeuronsCount;
 
+        /// <summary>
+        /// UI window - used for updating the GUI
+        /// </summary>
         private readonly MainWindow _mainWindow;
+
+        /// <summary>
+        /// data to claster
+        /// </summary>
+        public double[][] Data;
+
+        /// <summary>
+        /// num of rounds to learn
+        /// </summary>
+        public int NumEpoches;
+
+        /// <summary>
+        /// main window canvas refresh rate
+        /// </summary>
+        public int MainWindowRefreshRate; 
+
+        #endregion
 
         /// <summary>
         /// The constructor.
@@ -29,7 +62,7 @@ namespace SelfOrgenizedMap
         /// <param name="window">the gui window to notify</param>
         public SelfOrgnizedMap(int inputDimension, int numberOfClusters, MainWindow window)
         {
-            _inputNeuronsCount = inputDimension;
+            _inputDimension = inputDimension;
             _outputNeuronsCount = numberOfClusters;
 
             _mainWindow = window;
@@ -37,42 +70,47 @@ namespace SelfOrgenizedMap
             // initialize the weight matrix - and set all neurons at the center of the window
             _weights = new double[numberOfClusters][];
             for (var i = 0; i < numberOfClusters; i++)
-                _weights[i] = new double[]{50, 50};      
+                _weights[i] = new double[]{50, 50};   
+            
+            // initialize the biases for each neuron
+            _biases = new double[numberOfClusters];
         }
 
         /// <summary>
-        /// Start learning on the input data and update the main window 
+        /// Start learning the input data and update the main window 
         /// </summary>
-        /// <param name="inputData">the data to claster</param>
-        /// <param name="numOfEpoces">num of rounds to learn</param>
-        /// <param name="refreshRate">main window canvas refresh rate
-        ///                           TODO: add refresh points array 
-        /// </param>
-        public void Learn(double[][] inputData, int numOfEpoces, int refreshRate)
+        public void Learn()
         {
             var rand = new Random(DateTime.Now.Millisecond);
+            double learnRate = 1;
 
-            // start learning
-            for (int i = 0; i < numOfEpoces; i++)
+            // start learning - on every epoch we go over all the points in the train data
+            for (int i = 0; i < NumEpoches; i++)
             {
-                // update the gui
-                if (numOfEpoces%refreshRate == 0)
-                    _mainWindow.UpdateWindow(_weights);
+                // update the gui if necessery
+                if (i % MainWindowRefreshRate== 0)
+                    _mainWindow.UpdateWindow(_weights, learnRate, i);
 
+                // decrease the learn rate
+                learnRate = 1 - ((double)i/NumEpoches);
+                
                 // run over all the input data randomly
-                var alldata = inputData.ToList();
+                var alldata = Data.ToList();
                 while (alldata.Count != 0)
                 {
                     var input = alldata.ElementAt(rand.Next(alldata.Count));
                     alldata.Remove(input);
 
-                    // Find the cluster
+                    // Find the closest cluster
                     var neuronIdx = FindClosestNeuron(input);
+
+                    // update biases
+                    UpdateBiases(neuronIdx);
 
                     // change the neuron location by updating it's weights to be closer to the chosen input
                     for (var cluster = 0; cluster < _outputNeuronsCount; cluster++)
-                        for (int j = 0; j < _inputNeuronsCount; j++)
-                            ModifyWights(cluster, input, neuronIdx, i, function);
+                        for (int j = 0; j < _inputDimension; j++)
+                            ModifyWights(cluster, input, neuronIdx, learnRate);
                 }
             }
         }
@@ -90,7 +128,7 @@ namespace SelfOrgenizedMap
 
             for (var i = 0; i < _outputNeuronsCount; i++)
             {
-                var dist = EuclideanDistance(input, _weights[i]);
+                var dist = EuclideanDistance(input, _weights[i]) + _biases[i];
                 if (dist < minDist)
                 {
                     minDist = dist;
@@ -102,28 +140,60 @@ namespace SelfOrgenizedMap
         }
 
         /// <summary>
+        /// update the biases of the cluster neurons
+        /// Increasing the bias of the chosen neuron
+        /// Decreasing the bias of the other neurons
+        /// </summary>
+        /// <param name="chosenNeuronIdx"></param>
+        private void UpdateBiases(int chosenNeuronIdx)
+        {
+            const double biasChangeRate = 1;
+
+            for (int i = 0; i < _biases.Length; i++)
+            {
+                if (i != chosenNeuronIdx && _biases[i] != 0) _biases[i] -= biasChangeRate;
+                else _biases[i] += biasChangeRate;
+            }
+        }
+
+        /// <summary>
         /// Calculate the Euclidean distance between two vectors
         /// </summary>
         /// <param name="vec1">The first vector</param>
         /// <param name="vec2">The second vector</param>
         /// <returns>The distance.</returns>
-        private static double EuclideanDistance(IEnumerable<double> vec1, IList<double> vec2)
+        private static double EuclideanDistance(double[] vec1, double[] vec2)
         {
             return Math.Sqrt(vec1.Select((t, i) => t - vec2[i]).Sum(d => d * d));
         }
 
         /// <summary>
-        /// Updates the weight of the neuron according to the other parameters
+        /// Updates the weight of the neuron
         /// </summary>
         /// <param name="neuronToChangeIdx">the neuron which weights needs change</param>
         /// <param name="dataInput">data point which we want to cluster</param>
-        /// <param name="ClosestNeuronIdx">chosen closest neuron idx </param>
-        /// <param name="epochNum">epoch num - used to decrease the bonding between the Kohonen neurons on later epoches</param>
-        /// <param name="modificationFunction">the function which we use to modify the weights</param>
-        private void ModifyWights(int neuronToChangeIdx, double[] dataInput, int ClosestNeuronIdx, int epochNum,
-            ModificationFunction modificationFunction)
+        /// <param name="closestNeuronIdx">chosen closest neuron idx </param>
+        /// <param name="alpha">learn rate</param>
+        private void ModifyWights(int neuronToChangeIdx, double[] dataInput, int closestNeuronIdx, double alpha)
         {
-            
+            for (int i = 0; i < dataInput.Length; i++)
+                _weights[neuronToChangeIdx][i] += alpha*NeighberhoodFunction(neuronToChangeIdx, closestNeuronIdx)*
+                                                  (dataInput[i] - _weights[neuronToChangeIdx][i]);
+        }
+
+        /// <summary>
+        /// returns the amount of change according to the topology
+        /// </summary>
+        /// <param name="neuronToChange">the neuron which his weights are modified</param>
+        /// <param name="winnerNeuron">the neuron which represent the winner claster</param>
+        /// <returns>amoount of modification</returns>
+        private double NeighberhoodFunction(int neuronToChange, int winnerNeuron)
+        {
+            if (neuronToChange == winnerNeuron)
+                return 0.3;
+            if (Math.Abs(neuronToChange - winnerNeuron) == 1)
+                return 0.1;
+            return 0;
         }
     }
 }
